c68cbdbc2f8a988c0d82cfc300d555360df73c48
Amr Mostafa <amrmos.it@gmail.com>
2025-04-28 10:56:34 -0400

    Create/Activate/Test Django User Model

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '#Replace it with the generated password#',
+        'PASSWORD': 'Lf4h82Ycf0zUx5x3tpBGoyHM',
         'HOST': 'postgres',
         'PORT': '5432',
     }
